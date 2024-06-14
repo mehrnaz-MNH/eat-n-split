11581a7a71e4704d1917413d0a298bb39d42cbe2
mehrnaz-MNH <mnh.mehrnaz@gmail.com>
2024-06-15 00:33:36 +0330

    initial setup with sepration of concerns

---
+++ src/components/BillForm.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const BillForm = () => {
+    return (
+        <div>BillForm</div>
+    )
+}
---
+++ src/components/FriendItem.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const FriendItem = () => {
+    return (
+        <div>Item of the friend</div>
+    )
+}
